fd69d0dbbeda6d9b06993344305122e01cbb82d2
vvsjeiio <nomail>
2024-11-10 19:01:24 +0100

    Full responsive : adding small laptop

--- js/script.js
+++ js/script.js
@@ -1,58 +1,63 @@
 // DARK MODE -------------------
 
-const button = document.querySelector('.darkmode');
+const button = document.querySelectorAll('.darkmode');
 
-button.addEventListener('click', () => {
-    // Select image to swap
-    const darkMode = button.querySelector('.show');
-    const lightMode = button.querySelector('.hide');
+button.forEach(function (button) {
+    button.addEventListener('click', () => {
+        // Select image to swap
+        const darkMode = button.querySelector('.show');
+        const lightMode = button.querySelector('.hide');
 
-    // Invert show & hide class
-    darkMode.classList.remove('show');
-    darkMode.classList.add('hide');
+        // Invert show & hide class
+        darkMode.classList.remove('show');
+        darkMode.classList.add('hide');
 
-    lightMode.classList.remove('hide');
-    lightMode.classList.add('show');
+        lightMode.classList.remove('hide');
+        lightMode.classList.add('show');
 
-    // Swap main colors
-    const root = document.documentElement;
-    const currentBlack = getComputedStyle(root).getPropertyValue('--night-mode-black');
-    const currentWhite = getComputedStyle(root).getPropertyValue('--night-mode-white');
-    // Swap secondary colors
-    const secondaryBlack = getComputedStyle(root).getPropertyValue('--light-mode-black');
-    const secondaryWhite = getComputedStyle(root).getPropertyValue('--light-mode-white');
+        // Swap main colors
+        const root = document.documentElement;
+        const currentBlack = getComputedStyle(root).getPropertyValue('--night-mode-black');
+        const currentWhite = getComputedStyle(root).getPropertyValue('--night-mode-white');
+        // Swap secondary colors
+        const secondaryBlack = getComputedStyle(root).getPropertyValue('--light-mode-black');
+        const secondaryWhite = getComputedStyle(root).getPropertyValue('--light-mode-white');
 
-    // Set colors
-    root.style.setProperty('--night-mode-black', currentWhite.trim());
-    root.style.setProperty('--night-mode-white', currentBlack.trim());
-    root.style.setProperty('--light-mode-black', secondaryWhite.trim());
-    root.style.setProperty('--light-mode-white', secondaryBlack.trim());
+        // Set colors
+        root.style.setProperty('--night-mode-black', currentWhite.trim());
+        root.style.setProperty('--night-mode-white', currentBlack.trim());
+        root.style.setProperty('--light-mode-black', secondaryWhite.trim());
+        root.style.setProperty('--light-mode-white', secondaryBlack.trim());
 
 
-    //Change icon color
-    var icons = document.querySelectorAll(".icon");
-    icons.forEach(function (icon) {
-        // Switch order if landing mode is reversed
-        if (document.body.classList.contains("dark-mode")) {
-            icon.style.filter = "brightness(0%) saturate(0%)  invert(100%) brightness(98%)"; // White
+        //Change icon color
+        var icons = document.querySelectorAll(".icon");
+        icons.forEach(function (icon) {
+            // Switch order if landing mode is reversed
+            if (document.body.classList.contains("dark-mode")) {
+                icon.style.filter = "brightness(0%) saturate(0%)  invert(100%) brightness(98%)"; // White
 
-        } else {
-            icon.style.filter = "invert(10%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(90%) contrast(90%)"
-        }
+            } else {
+                icon.style.filter = "invert(10%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(90%) contrast(90%)"
+            }
+        });
+        document.body.classList.toggle("dark-mode");
     });
-    document.body.classList.toggle("dark-mode");
 });
 
 
 // DOWNLOAD BUTTON -----------
 
-document.getElementById("download-cv").addEventListener("click", function () {
-    const link = document.createElement("a");
-    link.href = "./data/CV_Baptiste_BLET_v4.pdf";
-    link.download = "CV_Baptiste_BLET(développeur web).pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+const download = document.querySelectorAll(".download-cv");
+download.forEach(function (download) {
+    download.addEventListener("click", function () {
+        const link = document.createElement("a");
+        link.href = "./data/CV_Baptiste_BLET_v4.pdf";
+        link.download = "CV_Baptiste_BLET(développeur web).pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
 });
 
 
